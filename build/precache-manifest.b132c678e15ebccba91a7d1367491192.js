252cd3c547393a05824ceb6bfe10b0dc99a79c9d
kassadube <noamsrb@yahoo.com>
2018-10-21 13:18:57 +0300

    init

---
+++ build/precache-manifest.b132c678e15ebccba91a7d1367491192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b5bca07d71b040013ac",
+    "url": "/static/js/main.5b5bca07.chunk.js"
+  },
+  {
+    "revision": "a2ac0061846e80fbb92a",
+    "url": "/static/js/1.a2ac0061.chunk.js"
+  },
+  {
+    "revision": "5b5bca07d71b040013ac",
+    "url": "/static/css/main.139c7a1f.chunk.css"
+  },
+  {
+    "revision": "0da385f94bf21d532d8d3fb51d2803f7",
+    "url": "/index.html"
+  }
+];
